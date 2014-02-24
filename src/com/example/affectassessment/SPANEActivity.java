dd3fb6ca8479b4b653f03a5137a8504544203be7
Minh Khue <lmk19922000@gmail.com>
2014-02-25 02:43:27 +0800

    Testing logic for SPANE no photo

--- src/com/example/affectassessment/SPANEActivity.java
+++ src/com/example/affectassessment/SPANEActivity.java
@@ -1,30 +1,16 @@
 package com.example.affectassessment;
 
-import java.util.ArrayList;
-
 import android.app.Activity;
-import android.app.ListActivity;
 import android.os.Bundle;
 
-public class SPANEActivity extends ListActivity{
+public class SPANEActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		 // 1. pass context and data to the custom adapter
-        MyAdapter adapter = new MyAdapter(this, generateData());
- 
-        //2. setListAdapter
-        setListAdapter(adapter);
+		
 	}
 	
-	 private ArrayList<Item> generateData(){
-	        ArrayList<Item> items = new ArrayList<Item>();
-	        items.add(new Item("Item 1","First Item on the list"));
-	        items.add(new Item("Item 2","Second Item on the list"));
-	        items.add(new Item("Item 3","Third Item on the list"));
 	 
-	        return items;
-	    }
 }
--- src/com/example/affectassessment/SPANENoPhotoActivity.java
+++ src/com/example/affectassessment/SPANENoPhotoActivity.java
@@ -1,17 +1,99 @@
 package com.example.affectassessment;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-public class SPANENoPhotoActivity extends Activity{
-
+public class SPANENoPhotoActivity extends Activity implements OnClickListener{
+	
+	Button btnSave, btnNote, btnShare;
+	
+	RadioGroup radioGroupPositive;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.activity_spane_no_photo);
 		
-		
+		initializeCompnentView();
 	}
-	
+
+	private void initializeCompnentView() {
+		btnSave = (Button) findViewById(R.id.buttonSaveSPANENoPhoto);
+		btnNote = (Button) findViewById(R.id.buttonNoteSPANENoPhoto);
+		btnShare = (Button) findViewById(R.id.buttonShareSPANENoPhoto);
+		
+		btnSave.setOnClickListener(this);
+		btnNote.setOnClickListener(this);
+		btnShare.setOnClickListener(this);
+		
+		radioGroupPositive = (RadioGroup) findViewById(R.id.radioGroupPositive);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		case R.id.buttonSaveSPANENoPhoto:
+			// TODO: Save to DB
+			int radioButtonID = radioGroupPositive.getCheckedRadioButtonId();
+			View radioButton = radioGroupPositive.findViewById(radioButtonID);
+			int idx = radioGroupPositive.indexOfChild(radioButton);
+			Log.i("Button selected", String.valueOf(idx));
+			break;
+		case R.id.buttonShareSPANENoPhoto:
+			// TODO: Open option to share by various means
+			break;
+		case R.id.buttonNoteSPANENoPhoto:
+			displayNoteDialog();
+			break;
+		}
+	}
+
+	private void displayNoteDialog() {
+		// get prompts.xml view
+		LayoutInflater li = LayoutInflater.from(this);
+		View promptsView = li.inflate(R.layout.prompt, null);
+
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+		// set prompts.xml to alertdialog builder
+		alertDialogBuilder.setView(promptsView);
+
+		final EditText userInput = (EditText) promptsView
+				.findViewById(R.id.editTextDialogUserInput);
+
+		// set dialog message
+		alertDialogBuilder
+			.setCancelable(false)
+			.setPositiveButton("OK",
+			  new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog,int id) {
+			    	
+			    // TODO: Save user input
+				//result.setText(userInput.getText());
+			    	
+			    }
+			  })
+			.setNegativeButton("Cancel",
+			  new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog,int id) {
+				dialog.cancel();
+			    }
+			  });
+
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+
+		// show it
+		alertDialog.show();	
+	}
 }
