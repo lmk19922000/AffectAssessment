1c590c6952b75733e649fca633b3bb771f54720c
Minh Khue <lmk19922000@gmail.com>
2014-03-22 03:29:39 +0800

    Finish draft of SPANE no photo

--- src/com/example/affectassessment/SPANENoPhotoActivity.java
+++ src/com/example/affectassessment/SPANENoPhotoActivity.java
@@ -12,18 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 
-public class SPANENoPhotoActivity extends Activity implements OnClickListener{
-	
+public class SPANENoPhotoActivity extends Activity implements OnClickListener {
+
 	Button btnSave, btnNote, btnShare;
-	
-	RadioGroup radioGroupPositive;
-	
+
+	RadioGroup radioGroupPositive, radioGroupNegative, radioGroupBad,
+			radioGroupGood, radioGroupPleasant, radioGroupUnpleasant,
+			radioGroupHappy, radioGroupSad, radioGroupAfraid, radioGroupJoyful,
+			radioGroupAngry, radioGroupContented;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.activity_spane_no_photo);
-		
+
 		initializeCompnentView();
 	}
 
@@ -31,17 +34,28 @@ public class SPANENoPhotoActivity extends Activity implements OnClickListener{
 		btnSave = (Button) findViewById(R.id.buttonSaveSPANENoPhoto);
 		btnNote = (Button) findViewById(R.id.buttonNoteSPANENoPhoto);
 		btnShare = (Button) findViewById(R.id.buttonShareSPANENoPhoto);
-		
+
 		btnSave.setOnClickListener(this);
 		btnNote.setOnClickListener(this);
 		btnShare.setOnClickListener(this);
-		
+
 		radioGroupPositive = (RadioGroup) findViewById(R.id.radioGroupPositive);
+		radioGroupNegative = (RadioGroup) findViewById(R.id.radioGroupNegative);
+		radioGroupGood = (RadioGroup) findViewById(R.id.radioGroupGood);
+		radioGroupBad = (RadioGroup) findViewById(R.id.radioGroupBad);
+		radioGroupPleasant = (RadioGroup) findViewById(R.id.radioGroupPleasant);
+		radioGroupUnpleasant = (RadioGroup) findViewById(R.id.radioGroupUnpleasant);
+		radioGroupSad = (RadioGroup) findViewById(R.id.radioGroupSad);
+		radioGroupHappy = (RadioGroup) findViewById(R.id.radioGroupHappy);
+		radioGroupAfraid = (RadioGroup) findViewById(R.id.radioGroupAfraid);
+		radioGroupJoyful = (RadioGroup) findViewById(R.id.radioGroupJoyful);
+		radioGroupAngry = (RadioGroup) findViewById(R.id.radioGroupAngry);
+		radioGroupContented = (RadioGroup) findViewById(R.id.radioGroupContented);
 	}
 
 	@Override
 	public void onClick(View v) {
-		switch(v.getId()){
+		switch (v.getId()) {
 		case R.id.buttonSaveSPANENoPhoto:
 			// TODO: Save to DB
 			int radioButtonID = radioGroupPositive.getCheckedRadioButtonId();
@@ -73,27 +87,26 @@ public class SPANENoPhotoActivity extends Activity implements OnClickListener{
 
 		// set dialog message
 		alertDialogBuilder
-			.setCancelable(false)
-			.setPositiveButton("OK",
-			  new DialogInterface.OnClickListener() {
-			    public void onClick(DialogInterface dialog,int id) {
-			    	
-			    // TODO: Save user input
-				//result.setText(userInput.getText());
-			    	
-			    }
-			  })
-			.setNegativeButton("Cancel",
-			  new DialogInterface.OnClickListener() {
-			    public void onClick(DialogInterface dialog,int id) {
-				dialog.cancel();
-			    }
-			  });
+				.setCancelable(false)
+				.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+
+						// TODO: Save user input
+						// result.setText(userInput.getText());
+
+					}
+				})
+				.setNegativeButton("Cancel",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								dialog.cancel();
+							}
+						});
 
 		// create alert dialog
 		AlertDialog alertDialog = alertDialogBuilder.create();
 
 		// show it
-		alertDialog.show();	
+		alertDialog.show();
 	}
 }
